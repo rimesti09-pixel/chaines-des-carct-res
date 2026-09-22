e64f6a7172c7f1527eab69144096bf398ed180c7
rimesti09-pixel <rimesti09@gmail.com>
2026-09-22 19:04:55 +0000

    Delete phrase.js

--- phrase.js
+++
@@ -1,8 +0,0 @@
-const prompt = require('prompt-sync')();
-let phrase = prompt("type your text")
-let pc = 0
-for (let caractere of phrase ){
-    pc++;
-}
-
-console.log(`la longueur de "${phrase}" est de ${pc} caractères.`)
